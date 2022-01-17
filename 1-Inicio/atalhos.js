d35283250afbc6f9dcfa45d036d10112af9baf93
Gilberto Filho <g.filho56@gmail.com>
2022-01-17 17:30:07 -0300

    Módulo 1 JS
    Adicionado conceito de blocos, uso de comentários e impressão no console.

---
+++ 1-Inicio/atalhos.js
@@ -0,0 +1,5 @@
+
+
+// alt + shift + baixo --> Duplica as linhas
+
+// shift + alt + S --> Formata o codigo
---
+++ 1-Inicio/comentarios.js
@@ -0,0 +1,9 @@
+
+// Teste de comentário
+console.log('Eu fiz um comentário acima.');
+
+/*
+Dicas de comentarios:
+1- Evite comentários óbvios.
+*/
+
