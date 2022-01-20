e4bbc3abf152b0f1d28d50807ab9930c25a77ff4
Gilberto Filho <contatogilbertofilho@gmail.com>
2022-01-20 17:04:32 -0300

    Módulo 1 JS
    Resolução de desafio.

--- 1-Inicio/desafios.js
+++ 1-Inicio/desafios.js
@@ -7,3 +7,14 @@ var resultado = PI * raio * raio;
 
 console.log("O raio dessa circunferência é " + resultado + "m²");
 
+// Troque os valores das variáveis A com a B:
+
+let a = 7;
+let b = 94;
+
+let temp = a;
+a = b;
+b = temp;
+
+console.log("O valor de a agora é " + a + " e o valor de b é " + b);
+
